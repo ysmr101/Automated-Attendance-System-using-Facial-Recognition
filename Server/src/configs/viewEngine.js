7f4ce39ad623d84fac534c35daa31e353eb73ad9
Abdulkarim Alluhaydan <105730614+AbdulkarimAli1@users.noreply.github.com>
2023-10-30 00:29:53 +0300

    Update viewEngine.js

--- Server/src/configs/viewEngine.js
+++ Server/src/configs/viewEngine.js
@@ -5,8 +5,7 @@ const express = require("express");
 let configViewEngine = (app)=> {
     app.use(express.static("./src")); 
     app.use(express.static("./src/public"));
-    app.set("view engine", "ejs");
-    app.set("views","./src/views");
+    
 };
 
 module.exports = configViewEngine;
